3b17c4854dfecfe31c81306d6890a463af3a7ddd
kalmary <siniarskim@gmail.com>
2026-01-30 14:41:45 +0100

    add training pipeline

--- src/utils/features.py
+++ src/utils/features.py
@@ -5,9 +5,9 @@ def superpoint_features(points, indices):
     centroid = P.mean(axis=0)
 
     Q = P - centroid
-    U, S, _ = np.linalg.svd(Q, full_matrices=False)
+    _, S, Vt = np.linalg.svd(Q, full_matrices=False)
 
-    pca_dir = U[:, 0]
+    pca_dir = Vt[0]
     verticality = abs(np.dot(pca_dir, np.array([0.0, 0.0, 1.0])))
     thickness = np.sqrt(S[1] * S[2])
     bbox_radius = 0.5 * np.linalg.norm(P.max(axis=0) - P.min(axis=0))
